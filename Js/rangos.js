16d5508db238ea9c726183f852587750202abb09
Aitor <91470257+Aitormon@users.noreply.github.com>
2024-04-13 19:19:39 +0200

    Script funcional

--- Js/rangos.js
+++ Js/rangos.js
@@ -1,11 +1,13 @@
 async function geteventos() {
     const url = new URL("https://valorant-api.com/v1/events");
-    url.searchParams.append("language", "es_ES");
+    //url.searchParams.append("language", "es_ES");
     try {
         const response = await fetch(url.toString());
         const data = await response.json();
         console.log(data);
-        createeventosLi(data.results)
+       // console.log(data.results);
+        //createeventosLi(data.data);
+        createeventoHtml(data.data);
     }
     catch (error) {
         console.error(error);
@@ -16,7 +18,7 @@ async function getevento(url) {
     try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log(data);
+        //console.log(data);
         createeventoHtml(data);
     }
     catch (error) {
@@ -24,34 +26,71 @@ async function getevento(url) {
     }
 }
 
-function createeventosLi(eventos) {
-    const eventosList = document.getElementById("displayName");
+/* function createeventosLi(eventos) {
+    const eventosList = document.getElementById("eventos");
+
     eventosList.innerHTML = "";
+   
     eventos.forEach(evento => {
         const li = document.createElement("li");
-        li.innerText = eventos.name;
+        li.innerText = eventos.shortDisplayName;
         li.addEventListener("click",()=>{
-            geteventos(eventos.url)
+            geteventos(eventos.assetPath)
         })
         eventosList.appendChild(li);
     });
-}
+} */
 
-function createeventoHtml(evento){
-    const eventoDiv = document.getElementById("displayName");
+function createeventoHtml(eventos){
+    const eventoDiv = document.getElementById("eventos");
     eventoDiv.innerHTML="";
-    const title = document.createElement("h2");
-    title.innerText=evento.name;
 
-    const typesList = document.createElement("ul");
-    for(const type of evento.types){
+    const ListaEventos = document.createElement("ul");
+
+    for(const evento of eventos) {
+
+        
+        let title = document.createElement("h2");
+        title.innerText="Nombre del evento : " + evento.shortDisplayName.toString().toUpperCase();
+        ListaEventos.appendChild(title);
+
+        title = document.createElement("h3");
+        title.innerText="Fecha de Inicio del evento"
+        ListaEventos.appendChild(title);
+
+        let li = document.createElement("li");
+        li.innerText = evento.startTime.replace("T"," , ");
+        li.innerText = li.innerText.replace("Z","");
+        ListaEventos.appendChild(li);
+
+        title = document.createElement("h3");
+        title.innerText="Fecha de Fin del evento"
+        ListaEventos.appendChild(title);
+
+
+
+        li = document.createElement("li");
+        li.innerText = evento.endTime.replace("T"," , ");
+        li.innerText = li.innerText.replace("Z","");
+        ListaEventos.appendChild(li);
+/*         li.addEventListener("click",()=>{
+            geteventos(eventos.assetPath)
+        }) */
+        
+        
+    };
+
+    eventoDiv.appendChild(ListaEventos);
+
+
+/*     for(const type of evento.types){
         const typeLi = document.createElement("li");
-        typeLi.innerText = type.type.name
+        typeLi.innerText = type.shortDisplayName;
         typesList.appendChild(typeLi);
-    }
-    eventoDiv.appendChild(title)
+    } */
+/*     eventoDiv.appendChild(title)
 
-    eventoDiv.appendChild(typesList)
+    eventoDiv.appendChild(typesList) */
 
 }
 
---
+++ README.md
@@ -0,0 +1,4 @@
+Herrera
+Dorleta
+
+Onlu
