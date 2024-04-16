75e76956c29d445a869f5f0a3b244d26a4d48b92
Aitor <91470257+Aitormon@users.noreply.github.com>
2024-04-16 15:27:38 +0200

    Version final

--- Js/eventos.js
+++ Js/eventos.js
@@ -9,8 +9,7 @@ async function geteventos() {
         data = res.data;
         //console.log(data);
        // console.log(data.results);
-        //createeventosLi(data.data);
-       // createeventoHtml(data.data);
+
        
     }
     catch (error) {
@@ -30,43 +29,110 @@ function muestraeventos(filtro){
     
     ListaEventos.appendChild(document.createElement("br"));
 
-    for(const evento of data) {
+    let encontrado = false;
+
+    
+    for(var evento of data) {
         //console.log(evento.shortDisplayName.toString());
         if (evento.shortDisplayName.toString().toLowerCase().includes(filtro.toLowerCase()) || evento.displayName.toString().toLowerCase().includes(filtro.toLowerCase())) {
+
+            encontrado = true;
+
+        }
+    }
+
+
+    if(encontrado === true){
+        let title = document.createElement("h2");
+
+        document.getElementById('galeria').style.display = 'none';
+        title.innerText="Evento encontrado!";
+        ListaEventos.appendChild(title);
+    }else{
+
+    alert("NO SE HA ENCONTRADO NINGUN EVENTO");
+        return;
+    }
+
+    
+    for(var evento of data) {
+     
+        if (evento.shortDisplayName.toString().toLowerCase().includes(filtro.toLowerCase()) || evento.displayName.toString().toLowerCase().includes(filtro.toLowerCase())) {
             
-            let title = document.createElement("h2");
-        title.innerText="Nombre del evento : " + evento.shortDisplayName.toString().toUpperCase();
-        ListaEventos.appendChild(title);
+        let title4 = document.createElement("h2");
+       title4.classList.add('titulo');
+        title4.innerText=evento.shortDisplayName.toString().toUpperCase() + " (" + evento.displayName.toString().toUpperCase() + ")";
+        ListaEventos.appendChild(title4);
+        
+    
+       /*  ListaEventos.appendChild(document.createElement("br")); */
 
-        title = document.createElement("h3");
-        title.innerText = evento.displayName.toString().toUpperCase();
-        ListaEventos.appendChild(title);
+        let title3 = document.createElement("h3");
+        title3.innerText="Fecha de inicio del evento";
+        title3.classList.add('titulo2');
+        ListaEventos.appendChild(title3);
 
-        title = document.createElement("h3");
-        title.innerText="Fecha de Inicio del evento"
-        ListaEventos.appendChild(title);
+        /* ListaEventos.appendChild(document.createElement("br")); */
 
         let li = document.createElement("li");
         li.innerText = evento.startTime.replace("T"," Hora: ");
         li.innerText = li.innerText.replace("Z","");
+        
         ListaEventos.appendChild(li);
 
-        title = document.createElement("h3");
-        title.innerText="Fecha de Fin del evento"
-        ListaEventos.appendChild(title);
+        /* ListaEventos.appendChild(document.createElement("br")); */
+
+
+        let title2 = document.createElement("h3");
+        title2.innerText="Fecha de Fin del evento";
+        title2.classList.add('titulo2');
+        ListaEventos.appendChild(title2);
+        
 
         li = document.createElement("li");
         li.innerText = evento.endTime.replace("T"," Hora: ");
         li.innerText = li.innerText.replace("Z","");
         ListaEventos.appendChild(li);
 
-        title.innerText="____________________________________________________"
-        ListaEventos.appendChild(title);
+        ListaEventos.appendChild(document.createElement("br"));
+
+
+
+///BOTON
+        let boton = document.createElement('button');
+    boton.textContent = 'VOLVER';
+    boton.id = 'volver';
+    boton.style.backgroundColor = 'red';
+    boton.style.color = 'white';
+/*     boton.style.padding = '10px';
+    boton.style.border = 'none';
+    boton.style.cursor = 'pointer'; */
+
+    boton.addEventListener('click', function() {
+        window.location.href = 'eventos.html';
+    });
+
+        ListaEventos.appendChild(boton);
+
+///BOTON
+
+
+
+
+
+        let linea = document.createElement("h3");
+        linea.innerText="____________________________________________________"
+        ListaEventos.appendChild(linea);
+
 
         }         
                 
        
     };
+
+
+
+   
 
     eventoDiv.appendChild(ListaEventos);
 
